212be312530a6c633b9ca8f8a68046140290b4cf
maurifk <mauricio.friss@fing.edu.uy>
2023-12-08 21:52:03 -0300

    Update 09_94_TreeInorderTraversal.java

--- December_Daily_LeetCode/09_94_TreeInorderTraversal.java
+++ December_Daily_LeetCode/09_94_TreeInorderTraversal.java
@@ -1,16 +1,43 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 import Extra_Classes.TreeNode;
+
 
 class Solution {
     // Recursive solution
-    public List<Integer> inorderTraversal(TreeNode root) {
+    public List<Integer> inorderTraversalRec(TreeNode root) {
         if (root == null)
             return new LinkedList<Integer>();
-        List<Integer> res = inorderTraversal(root.left);
+        List<Integer> res = inorderTraversalRec(root.left);
         res.add(root.val);
-        res.addAll(inorderTraversal(root.right));
+        res.addAll(inorderTraversalRec(root.right));
         return res;
     }
 
+    // Iterative solution
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null)
+            return new LinkedList<Integer>();
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        List<Integer> res = new LinkedList<Integer>();
+
+        TreeNode curr = root;
+        while (true) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            if (stack.isEmpty())
+                break;
+
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+
+        return res;
+    }
 }
